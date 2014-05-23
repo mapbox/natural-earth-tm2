ff24d2de4bb5447ae5e807d4b61d8ea51453b654
AJ Ashton <aj.ashton@gmail.com>
2014-05-23 15:09:13 -0400

    derive points for state and country labels

--- util/setup_db.sh
+++ util/setup_db.sh
@@ -31,11 +31,36 @@ PGCLIENTENCODING=LATIN1 ogr2ogr \
     -f Postgresql \
     -s_srs EPSG:4326 \
     -t_srs EPSG:3857 \
-    -clipsrc -180 -85.0511 180 85.0511 \
+    -clipsrc -180.1 -85.0511 180.1 85.0511 \
     PG:"dbname=$dbname user=$dbuser host=$dbhost port=$dbport" \
     -lco GEOMETRY_NAME=geom \
     -lco DIM=2 \
     -nlt GEOMETRY \
     "$tmp/natural_earth_vector.sqlite"
+
+echo "Post-processing..."
+
+# Because polygons can get split up between many vector tiles, in order to
+# draw labels we'll want to pre-calculate label positions for polygons as
+# points. This section creates additional geometry columns for polygon 
+# layers we'll want to label, plus additional indexes.
+#
+# This could also be done as part of the SQL queries in the TM2 project, but
+# doing it beforehand cuts down on export time.
+
+tables=('ne_110m_admin_0_countries'
+        'ne_50m_admin_0_countries'
+        'ne_10m_admin_0_countries'
+        'ne_110m_admin_1_states_provinces_shp'
+        'ne_50m_admin_1_states_provinces_shp'
+        'ne_10m_admin_1_states_provinces_shp'
+    )
+
+for table in ${tables[@]}; do
+    $psql --single-transaction --dbname=$dbname --quiet --command="
+        ALTER TABLE $table ADD COLUMN geom_point geometry(point);
+        UPDATE $table SET geom_point = ST_PointOnSurface(geom);
+        CREATE INDEX ${table}_geom_point ON $table USING gist (geom_point);"
+done
 
 echo "Done."
